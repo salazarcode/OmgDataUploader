669ae2469075a0d9bda618261df4cf45fd0c5589
Andrés <andresalteclado@hotmail.com>
2022-05-12 17:59:51 -0500

    Elegant and beautiful structure for NETCORE application.

---
+++ DataProvider/PostgresProvider.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class PostgresProvider : IDataProvider
+    {
+        private readonly IConfiguration _conf;
+        protected string _connectionString { get; set; }
+        protected IDbConnection _conection { get; set; }
+
+        public PostgresProvider(IConfiguration conf)
+        {
+            _conf = conf;
+            _connectionString = _conf.GetConnectionString("postgres");
+        }
+
+        public IDbConnection GetConnection() 
+        {
+            IDbConnection connection = new NpgsqlConnection(_connectionString);
+            return connection;
+        }
+    }
+}
---
+++ Repository/DataProviderOneRepository.cs
@@ -0,0 +1,32 @@
+﻿using Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class DataProviderOneRepository : IDataProviderOneRepository
+    {
+        private readonly IDbConnection _connection;
+        public DataProviderOneRepository(IDataProvider dataProvider)
+        {
+            _connection = dataProvider.GetConnection();                
+        }
+        public int Create()
+        {
+            try
+            {
+
+                return 1;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+    }
+}
