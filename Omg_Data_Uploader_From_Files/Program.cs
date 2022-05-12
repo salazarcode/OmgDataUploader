397c660d4de8e0717ef2a34250690f34b42591a3
Andrés <andresalteclado@hotmail.com>
2022-05-12 15:48:39 -0500

    Agregar archivos de proyecto.

---
+++ Omg_Data_Uploader_From_Files/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Omg_Data_Uploader_From_Files
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
